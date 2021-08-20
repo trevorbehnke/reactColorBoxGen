cd66b1693d09fe051e9685ea09ff059ae8e11746
trevor <me@trevorbehnke.com>
2021-08-19 22:13:28 -0700

    initial commit

---
+++ src/BoxList.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Box from "./Box";
+import NewBoxForm from "./NewBoxForm";
+import "./BoxList.css";
+
+export default class BoxList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { boxes: [] };
+    this.create = this.create.bind(this);
+  }
+
+  remove(id) {
+    this.setState({ boxes: this.state.boxes.filter((box) => box.id !== id) });
+  }
+
+  create(newBox) {
+    this.setState({
+      boxes: [...this.state.boxes, newBox],
+    });
+  }
+
+  render() {
+    const boxes = this.state.boxes.map((box) => (
+      <Box
+        key={box.id}
+        id={box.id}
+        width={box.width}
+        height={box.height}
+        color={box.color}
+        removeBox={() => {
+          this.remove(box.id);
+        }}
+      />
+    ));
+    return (
+      <div className="container">
+        <h1>Color Box Maker Thingy</h1>
+        <NewBoxForm createBox={this.create} />
+        {boxes}
+      </div>
+    );
+  }
+}
